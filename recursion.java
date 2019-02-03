83a0d705cb76e648234f895e1502f888e753678e
tanzimelahi <telahi00@stuy.edu>
2019-02-02 19:58:33 -0500

    makeAllSums helper

--- recursion.java
+++ recursion.java
@@ -14,6 +14,18 @@ public class recursion{
   public static double sqrt(double n,double tolerance) {
 		return sqrtHelp(n,n/2,tolerance);
 	}
+  public static boolean makeAllSums(int n,int sum,ArrayList<Integer> result){
+    if(n==0) {
+     System.out.println(sum);
+     result.add(sum);
+     return  false;
+
+    }
+    else {
+      return makeAllSums(n-1,sum+n,result) || makeAllSums(n-1,sum,result);
+    }
+
+  }
   public static void main(String[]args){
     System.out.println(sqrt(0.04,0.0001));
 		System.out.println(sqrt(4,0.01));
